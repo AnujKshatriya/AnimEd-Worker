98c32f4d1d7e098cbd7ba6e5a2b50d7516bc3dd9
Anuj Kshatriya <anujkshatriyaiitian@gmail.com>
2025-10-25 19:03:57 +0530

    updated frontend url cors issue

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-  origin: 'http://localhost:8080', // frontend origin
+  origin: process.env.FRONTEND_URL, // frontend origin
   methods: ['GET','POST','PUT','DELETE','OPTIONS'],
   credentials: true
 }));
